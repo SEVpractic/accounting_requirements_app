637fb3178cd224b6c1906d08633563d99905309e
SEVpractic <wisebrain@yandex.ru>
2023-04-14 19:48:00 +0300

    feat: Создать сущность материал.

---
+++ src/main/java/sev_castom/accounting_requirements_app/model/Material.java
@@ -0,0 +1,25 @@
+package sev_castom.accounting_requirements_app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "materials")
+@Setter
+@Getter
+public class Material {
+    @Id
+    @Column(name = "material_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit")
+    private String unit;
+    @Column(name = "amount")
+    private String amount;
+}
